f5ebd8dcae6383e667666c82f8b1f00c580f1973
Jayakumar Jayaraman <jayakumar.jayaraman@eba.europa.eu>
2019-03-15 16:02:29 +0000

    grunt added

---
+++ hello/Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/**/*.js',
+                dest: 'build/main.min.js'
+            }
+        },
+        cssmin : {
+            options : {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            target : {
+              src: 'src/**/*.css'  ,
+              dest : 'build/style.min.css'
+            }
+        }        
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
+    grunt.registerTask('default', ['uglify','cssmin']);
+
+}
